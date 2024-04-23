0ef915263a95901060280fdb6c29bbddcdc46132
Minji <minji5064@gmail.com>
2024-04-23 23:59:34 +0900

    Time: 125 ms (80.12%), Space: 57.3 MB (58.75%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = function(x) {
+  const str = x.toString()
+  let start = 0
+  let length = str.length - 1
+
+  while(start < length){
+    if(str[start] !== str[length]) return false
+    start += 1;
+    length -= 1;
+  }
+
+  return true
+};
